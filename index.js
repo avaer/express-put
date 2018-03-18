6a6e028267315f1b539279a82d9b03a0f542b176
Avaer Kazmer <a@modules.io>
2018-03-18 06:00:18 -0700

    Add mountPath support

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = express => {
-  const put = dirname => {
+  const put = (dirname, mountPath = '') => {
     const staticServer = express.static();
 
     return (req, res, next) => {
       if (req.method === 'PUT') {
-        const filePath = path.join(dirname, req.path);
-        const ws = fs.createWriteStream(filePath);
-        req.pipe(ws);
-        ws.on('finish', () => {
-          res.end();
-        });
-        ws.on('error', err => {
-          res.status(500);
-          res.end(http.STATUS_CODES[500]);
-        });
+        if (req.path.indexOf(mountPath) === 0) {
+          const filePath = path.join(dirname, req.path.slice(mountPath.length));
+          const ws = fs.createWriteStream(filePath);
+          req.pipe(ws);
+          ws.on('finish', () => {
+            res.end();
+          });
+          ws.on('error', err => {
+            res.status(500);
+            res.end(http.STATUS_CODES[500]);
+          });
+        } else {
+          next();
+        }
       } else {
         staticServer(req, res, next);
       }
